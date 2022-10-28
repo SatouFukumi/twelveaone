3ecf5e5b008b68192a5fb277ddffb49f624030cc
Fukumi <satoufukumii@gmail.com>
2022-10-29 00:33:22 +0700

    🧭

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ const nextConfig = {
     sassOptions: { includePaths: ['src/styles', 'assets/styles'] },
 
     env: {
-        DB_API_KEY: process.env.DB_API_KEY,
-        DB_AUTH_DOMAIN: process.env.DB_AUTH_DOMAIN,
-        DB_PROJECT_ID: process.env.DB_PROJECT_ID,
-        DB_STORAGE_BUCKET: process.env.DB_STORAGE_BUCKET,
-        DB_MESSAGING_SENDER_ID: process.env.DB_MESSAGING_SENDER_ID,
-        DB_APP_ID: process.env.DB_APP_ID,
-        DB_MEASUREMENT_ID: process.env.DB_MEASUREMENT_ID
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
     }
 }
 
