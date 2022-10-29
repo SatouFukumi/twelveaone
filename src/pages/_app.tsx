d31c2b4e187eaf4b60877f47b2fbd2ac9e7bfa2c
Fukumi <satoufukumii@gmail.com>
2022-10-29 19:06:52 +0700

    👾

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,29 @@
-import NextApp from "next/app"
+import { AppType } from "next/app"
 import { trpc } from "src/backend/api/trpc"
 import { Tooltip } from "assets/components/tooltip"
+import Container from "components/app/container"
+import Ruler from "components/app/ruler"
+import Navigation from "components/app/navigation"
 
 // stylesheets
 import "assets/styles/main.scss"
 import "src/styles/globals.scss"
 import layout from 'src/styles/layout.module.scss'
-import Container from "components/app/container"
-import Ruler from "components/app/ruler"
-import Navigation from "components/app/navigation"
 
-class App extends NextApp {
-    render(): JSX.Element {
-        const { Component, pageProps } = this.props
+const App: AppType = ({ Component, pageProps }) => {
+    return (
+        <section id={layout.app}>
+            {/* helpers */}
+            <Tooltip />
 
-        return (
-            <section id={layout.app}>
-                {/* helpers */}
-                <Tooltip />
-
-                {/* main content */}
-                <Ruler id={layout.ruler} />
-                <Navigation id={layout.navigation} />
-                <Container id={layout.container}>
-                    <Component {...pageProps} />
-                </Container>
-            </section>
-        )
-    }
+            {/* main content */}
+            <Ruler id={layout.ruler} />
+            <Navigation id={layout.navigation} />
+            <Container id={layout.container}>
+                <Component {...pageProps} />
+            </Container>
+        </section>
+    )
 }
 
 export default trpc.withTRPC(App)
