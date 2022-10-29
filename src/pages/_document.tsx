97327e17749705a13145e946706e295fd79a8f2e
Fukumi <satoufukumii@gmail.com>
2022-10-29 19:13:24 +0700

    🖖

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import NextDocument, { Main, NextScript, Head, Html } from "next/document"
+import { Main, NextScript, Head, Html } from "next/document"
 
-export default class Document extends NextDocument {
-    render(): JSX.Element {
-        return (
-            <Html lang='vi'>
-                <Head>
-                    <meta charSet="utf-8" />
-                </Head>
+const Document = () => {
+    return (
+        <Html lang="vi">
+            <Head>
+                <meta charSet="utf-8" />
+            </Head>
 
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
+
+export default Document
