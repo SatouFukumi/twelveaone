9de61a7c494a5186885b701b3530599e56bb134d
Fukumi <satoufukumii@gmail.com>
2022-11-02 15:45:13 +0700

    💻

--- src/components/app/splash.tsx
+++ src/components/app/splash.tsx
@@ -32,7 +32,7 @@ const Splash: React.FC = () => {
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                     viewBox="0 24 150 28"
                     preserveAspectRatio="none"
-                    shape-rendering="auto"
+                    shapeRendering="auto"
                 >
                     <defs>
                         <path
